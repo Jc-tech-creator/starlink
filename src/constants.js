894b5f1cef2c4cd2451ef766f154a86596d837d0
Jiacheng Yu <jyu96203@usc.edu>
2022-09-08 16:39:17 -0700

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "VP3FGE-P56FN9-6XDMJE-4WF3";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://18.219.114.62";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
